c8f407fb3b96f263b0a97206ed58dac1b0c94d3c
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2026-04-27 23:10:40 +0300

    sanity

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,6 +1,7 @@
-import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity.config";
+import dynamic from "next/dynamic";
+
+const StudioClient = dynamic(() => import("@/components/StudioClient"), { ssr: false });
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <StudioClient />;
 }
