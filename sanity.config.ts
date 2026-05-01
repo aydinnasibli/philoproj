29d20de1c662e9a54ebc50ee0b6873d3c773557a
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2026-05-01 13:44:17 +0300

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@ import { schemaTypes } from "./schemaTypes";
 export default defineConfig({
   name: "default",
   title: "The Living Manuscript",
+  basePath: "/studio",
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset:   process.env.NEXT_PUBLIC_SANITY_DATASET!,
